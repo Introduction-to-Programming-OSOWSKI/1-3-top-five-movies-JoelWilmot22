179c59df1a266256b005b88230f4a4eed015ffc5
Wilmot Joel <818032@stma.k12.mn.us>
2021-03-25 10:24:23 -0500

    Try 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + e
+def topFiveMovies(a , b , c , d , e):
+    return a + " " + b + " " + c + " " + d + " " + e
 
-print(topFiveMovies("The Dark Knight, ", "The Shawshank Redemption, ", "The Empire Strikes Back, ", "Raiders of The Lost Ark, ", "and Back to The Future."))
+print(topFiveMovies("The Dark Knight,", "The Shawshank Redemption,", "The Empire Strikes Back,", "Raiders of The Lost Ark,", "and Back to The Future."))
 
