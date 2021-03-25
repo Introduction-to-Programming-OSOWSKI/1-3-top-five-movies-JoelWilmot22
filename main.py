be09a0a97b965c3ade7b0206e93443ab9f6c159b
Wilmot Joel <818032@stma.k12.mn.us>
2021-03-25 10:09:57 -0500

    Top Five Movies Try 1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a,b,c,d,e):
+    return a + b + c + d + e
+
+print(topFiveMovies("The Dark Knight, ", "The Shawshank Redemption, ", "The Empire Strikes Back, ", "Raiders of The Lost Ark, ", "and Back to The Future."))
+
